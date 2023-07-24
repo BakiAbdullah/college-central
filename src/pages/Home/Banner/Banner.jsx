35f036a7f8ca54c43a1adc3e44bba692595f7a85
BakiAbdullah <shatil605@gmail.com>
2023-07-24 23:29:13 +0600

    banner title fixed

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -58,7 +58,7 @@ const Banner = () => {
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="relative max-w-2xl sm:mx-auto sm:max-w-xl md:max-w-2xl sm:text-center">
             <h2 className="mb-8 font-extrabold lg:text-4xl tracking-tight text-white  sm:leading-none">
-              Welcome to the College Stay
+              Welcome to the College Central Stay
               <br className="hidden md:block" />
               Revolution, Book{" "}
               <span className="relative inline-block px-2">
